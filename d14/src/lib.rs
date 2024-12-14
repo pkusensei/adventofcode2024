93a0dd62772c472b63de0971900d667f2d9c5ea2
pkusensei <ZW_08@outlook.com>
2024-12-14 09:14:02 -0500

    1214 a tree!

---
+++ d14/src/lib.rs
@@ -0,0 +1,87 @@
+use std::{str, sync::LazyLock};
+
+use bmp::Image;
+use regex::Regex;
+
+static RE: LazyLock<Regex> =
+    LazyLock::new(|| Regex::new(r#"p=(-*\d+),(-*\d+) v=(-*\d+),(-*\d+)"#).unwrap());
+
+pub fn p1(s: &str, is_test: bool) -> u64 {
+    let [xmax, ymax] = if is_test { [11, 7] } else { [101, 103] };
+    let [mut a, mut b, mut c, mut d] = [0; 4];
+    for [mut x, mut y, dx, dy] in parse(s) {
+        x += 100 * dx;
+        y += 100 * dy;
+        x = x.rem_euclid(xmax);
+        y = y.rem_euclid(ymax);
+        if (0..xmax / 2).contains(&x) && (0..ymax / 2).contains(&y) {
+            a += 1;
+        }
+        if (1 + xmax / 2..xmax).contains(&x) && (0..ymax / 2).contains(&y) {
+            b += 1;
+        }
+        if (0..xmax / 2).contains(&x) && (1 + ymax / 2..ymax).contains(&y) {
+            c += 1;
+        }
+        if (1 + xmax / 2..xmax).contains(&x) && (1 + ymax / 2..ymax).contains(&y) {
+            d += 1;
+        }
+    }
+    a * b * c * d
+}
+
+pub fn p2(s: &str) -> bool {
+    let [xmax, ymax] = [101, 103];
+    let mut points: Vec<_> = parse(s).collect();
+    for i in 0..xmax * ymax {
+        let mut img = Image::new(xmax as _, ymax as _);
+        for p in points.iter_mut() {
+            p[0] += p[2];
+            p[1] += p[3];
+            p[0] = p[0].rem_euclid(xmax);
+            p[1] = p[1].rem_euclid(ymax);
+            img.set_pixel(p[0] as _, p[1] as _, bmp::consts::WHITE);
+        }
+        img.save(format!("../.img/{:05}.bmp", i)).expect("???");
+    }
+    true
+}
+
+fn parse(s: &str) -> impl Iterator<Item = [i32; 4]> + '_ {
+    s.lines().map(|line| {
+        let (_, v) = RE.captures(line).unwrap().extract();
+        v.map(|n| n.parse().unwrap())
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST: &str = r#"p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3"#;
+
+    const INPUT: &str = include_str!("../input.txt");
+
+    #[test]
+    fn t1() {
+        assert_eq!(p1(TEST, true), 12);
+        assert_eq!(p1(INPUT, false), 224438715);
+    }
+
+    #[test]
+    #[ignore = "not generating 10k+ bmp's anymore"]
+    fn t2() {
+        assert!(p2(INPUT)); // 7603
+    }
+}
